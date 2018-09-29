a9336f422bffe074f7a89c777f7a25165142238e
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-09-28 23:50:07 -0500

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let pokemon = args[0]
-  let pEmbed = new Discord.RichEmbed();
+  let pEmbed = new Discord.RichEmbed()
   .setTitle("Here is your pokemon!")
   .setColor('RANDOM')
   .setThumbnail(`http://www.pokestadium.com/sprites/xy/`+ pokemon +`.gif`);
