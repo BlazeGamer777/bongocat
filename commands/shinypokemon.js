1b9ba44ac2cbd12d68d3b721af82fd716c22fdf4
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-09-28 23:56:23 -0500

    Update shinypokemon.js

--- commands/shinypokemon.js
+++ commands/shinypokemon.js
@@ -2,8 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let pokemon = args[0]
+  let pEmbed = new Discord.RichEmbed()
+  .setTitle("Here is your shiny pokemon!")
+  .setColor('RANDOM')
+  .addField("Name:", pokemon)
+  .setThumbnail(`http://www.pokestadium.com/sprites/xy/shiny/`+ pokemon +`.gif`);
 
-  message.channel.send(`http://www.pokestadium.com/sprites/xy/shiny/`+ pokemon +`.gif`);
+  message.channel.send(pEmbed);
 }
 
 module.exports.help = {
