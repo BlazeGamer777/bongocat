b5b8ebb1455001bd4a2ca2a9ad26368935fac8a4
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-10-10 19:04:35 -0500

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,19 +1,18 @@
 const Discord = require("discord.js");
-const superagent = require("superagent");
+const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-  let {body} = await superagent
-  .get(`https://api-to.get-a.life/meme`);
-
-  let memeembed = new Discord.RichEmbed()
-  .setColor("#ff9900")
-  .setTitle("Here's a meme 👌")
-  .setImage(body.url)
-  .setDescription(body.text);
-
-  message.channel.send(memeembed);
+  randomPuppy('dankmemes')
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
 }
 
 module.exports.help = {
   name: "meme"
-}
+};
