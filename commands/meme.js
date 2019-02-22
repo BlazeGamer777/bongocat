b89c6b7497ef7a2acda16ffb03b92230524e5f8e
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2019-02-21 18:17:19 -0600

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    randomPuppy('memes')
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setTitle("Heres a meme!")
+                .setColor("RANDOM")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+}
+
+module.exports.help = {
+  name: "meme"
+};
